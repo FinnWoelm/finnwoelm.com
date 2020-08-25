6a04d68a23a803031e3f5e4554382e10d3286782
Finn Woelm <public@finnwoelm.com>
2020-08-25 11:17:10 +0200

    Set up sanity.io CMS
    Set up headless content management system with sanity.io.

---
+++ studio/schemas/category.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'label',
+      title: 'Label',
+      type: 'string'
+    },
+    {
+      name: 'color',
+      title: 'Color',
+      type: 'string'
+    }
+  ]
+}
