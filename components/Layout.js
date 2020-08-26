f4d6b8b0f3e85253d5fff21bfb82e610d1f39095
Finn Woelm <public@finnwoelm.com>
2020-08-26 16:34:37 +0200

    Footer: Always stick footer to bottom by using flex layout
    Use flexGrow: 1; on the main content to keep the footer at the bottom of 
    the screen even when the content is short.

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,23 @@
+import { Box } from '@material-ui/core'
+import { createGlobalStyle } from 'styled-components'
 import NavBar from 'components/NavBar'
 import Footer from 'components/Footer'
 
+const GlobalStyle = createGlobalStyle`
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+`
+
 const Layout = ({ children }) => (
   <>
+    <GlobalStyle />
     <NavBar />
-    {children}
+    <Box flexGrow={1}>
+      {children}
+    </Box>
     <Footer />
   </>
 )
