705a149d0b0e1e2360cdb5b469661d8ba1ee6463
Finn Woelm <public@finnwoelm.com>
2020-08-26 16:21:36 +0200

    ExternalLink: Add support for ref
    The ExternalLink component needs to be able to hold a ref for working 
    with MUI components. This commit adds this support.

--- components/ExternalLink.js
+++ components/ExternalLink.js
@@ -1,7 +1,9 @@
-const ExternalLink = ({ children, href, ...otherProps }) => (
-  <a href={href} target="_blank" rel="noopener noreferrer" {...otherProps}>
+import { forwardRef } from 'react'
+
+const ExternalLink = forwardRef(({ children, href, ...otherProps }, ref) => (
+  <a href={href} ref={ref} target="_blank" rel="noopener noreferrer" {...otherProps}>
     {children}
   </a>
-)
+))
 
 export default ExternalLink
