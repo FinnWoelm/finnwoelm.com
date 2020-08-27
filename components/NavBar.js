e63d856e5052c12ff54458832bd8ec10e1d01fcb
Finn Woelm <public@finnwoelm.com>
2020-08-27 08:15:41 +0200

    Add mobile navigation menu
    On mobile, display hamburger icon to toggle navigation drawer.

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,47 +1,88 @@
+import { useState } from 'react'
 import Link from 'next/link'
-import { AppBar, Box, Button, Container, IconButton, Toolbar } from '@material-ui/core'
-import { Github, Linkedin, Twitter } from 'mdi-material-ui'
+import { AppBar, Box, Button, Container, Hidden, IconButton, Toolbar } from '@material-ui/core'
+import { Menu } from 'mdi-material-ui'
 import styled from 'styled-components'
+import theme from 'helpers/theme'
 import ExternalLink from 'components/ExternalLink'
+import NavBarDrawer from 'components/NavBarDrawer'
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from 'components/SocialIcons'
+
+const TWITTER = "https://twitter.com/FinnWoelm"
+const LINKEDIN = "https://www.linkedin.com/in/FinnWoelm/"
+const GITHUB = "https://github.com/FinnWoelm"
+
+const LeftAlignedToolbar = styled(Toolbar)`
+  ${theme.breakpoints.up('md')} {
+     margin-left: -8px;
+     margin-right: -8px;
+  }
+`
+
+const HiddenFlex = styled(Hidden)`
+  display: flex;
+`
 
 const SocialButton = styled(IconButton).attrs({
   component: ExternalLink
 })``
 
-const NavBar = () => (
-  <AppBar position='static' color='transparent' elevation={0} style={{ borderBottom: '1px solid #eee' }}>
-    <Container>
-      <Toolbar style={{ marginLeft: -32, marginRight: -32 }}>
-        <Link href="/" passHref>
-          <Button>
-            <img src='/logo.png' style={{ height: 30 }} />
-          </Button>
-        </Link>
-        <Box marginX={2} />
-        <Link href="/posts" passHref>
-          <Button color="inherit">
-            News
-          </Button>
-        </Link>
-        <Box marginX={2} />
-        <SocialButton
-          href="https://twitter.com/FinnWoelm"
-          style={{ color: '#1da1f2' }}>
-          <Twitter />
-        </SocialButton>
-        <SocialButton
-          href="https://www.linkedin.com/in/FinnWoelm/"
-          style={{ color: '#2867b2' }}>
-          <Linkedin />
-        </SocialButton>
-        <SocialButton
-          href="https://github.com/FinnWoelm"
-          style={{ color: '#000' }}>
-          <Github />
-        </SocialButton>
-      </Toolbar>
-    </Container>
-  </AppBar>
-)
+const NavBar = () => {
+
+  const [showNavDrawer, setShowNavDrawer] = useState(false)
+  const toggleNavDrawer = () => setShowNavDrawer(!showNavDrawer)
+
+  return (
+    <AppBar position='static' color='transparent' elevation={0} style={{ borderBottom: '1px solid #eee' }}>
+      <Container>
+        <LeftAlignedToolbar disableGutters>
+          <Hidden implementation='css' mdUp>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleNavDrawer}>
+              <Menu />
+            </IconButton>
+          </Hidden>
+          <Link href="/" passHref>
+            <Button>
+              <img src='/logo.png' style={{ height: 30 }} />
+            </Button>
+          </Link>
+          <Hidden implementation='js' mdUp>
+            <NavBarDrawer
+              open={showNavDrawer}
+              onClose={toggleNavDrawer}
+              twitter={TWITTER}
+              linkedin={LINKEDIN}
+              github={GITHUB} />
+          </Hidden>
+          <HiddenFlex implementation='css' smDown>
+            <Box marginX={2} />
+            <Link href="/posts" passHref>
+              <Button color="inherit">
+                News
+              </Button>
+            </Link>
+            <Box marginX={2} />
+            <SocialButton
+              href={TWITTER}>
+              <TwitterIcon />
+            </SocialButton>
+            <SocialButton
+              href={LINKEDIN}>
+              <LinkedInIcon />
+            </SocialButton>
+            <SocialButton
+              href={GITHUB}>
+              <GitHubIcon />
+            </SocialButton>
+          </HiddenFlex>
+        </LeftAlignedToolbar>
+      </Container>
+    </AppBar>
+  )
+}
 
 export default NavBar
---
+++ components/NavBarDrawer.js
@@ -0,0 +1,56 @@
+import Link from 'next/link'
+import { Box, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import styled from 'styled-components'
+import ExternalLink from 'components/ExternalLink'
+import { TwitterIcon, LinkedInIcon, GitHubIcon } from 'components/SocialIcons'
+
+const UnpaddedListItemIcon = styled(ListItemIcon)`
+ && {
+   min-width: 32px;
+ }
+`
+
+const NavBarDrawer = ({ open, onClose, twitter, linkedin, github }) => (
+  <Drawer
+    open={open}
+    onClose={onClose}>
+    <List>
+      <Link href="/" passHref>
+        <ListItem button component='a'>
+          <img src="/logo.png" style={{ maxWidth: '100%', padding: '16px 0' }}/>
+        </ListItem>
+      </Link>
+    </List>
+    <Divider />
+    <List>
+      <Link href="/posts" passHref>
+        <ListItem button component='a'>
+          <ListItemText primary="News" />
+        </ListItem>
+      </Link>
+    </List>
+    <Divider />
+    <List>
+      <ListItem button component={ExternalLink} href={twitter}>
+        <UnpaddedListItemIcon>
+          <TwitterIcon />
+        </UnpaddedListItemIcon>
+        <ListItemText primary="Twitter" />
+      </ListItem>
+      <ListItem button component={ExternalLink} href={linkedin}>
+        <UnpaddedListItemIcon>
+          <LinkedInIcon />
+        </UnpaddedListItemIcon>
+        <ListItemText primary="LinkedIn" />
+      </ListItem>
+      <ListItem button component={ExternalLink} href={github}>
+        <UnpaddedListItemIcon>
+          <GitHubIcon />
+        </UnpaddedListItemIcon>
+        <ListItemText primary="GitHub" />
+      </ListItem>
+    </List>
+  </Drawer>
+)
+
+export default NavBarDrawer
