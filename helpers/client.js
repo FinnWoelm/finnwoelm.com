d1b7eb90031161498e059339ff7823c29d681ecd
Finn Woelm <public@finnwoelm.com>
2021-07-02 18:49:53 +0200

    Sanity: Explicitly use API v1
    The latest API version uses a slightly different format and requires a 
    rewrite of the query.js helper.

--- helpers/client.js
+++ helpers/client.js
@@ -3,6 +3,8 @@ const sanityClient = require("@sanity/client");
 const client = sanityClient({
   projectId: "zdrr72ub",
   dataset: "production",
+  apiVersion: "v1",
+  token: process.env.SANITY_TOKEN,
   useCdn: false, // `false` if you want to ensure fresh data
 });
 
